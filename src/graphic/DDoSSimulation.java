72ab60ff80d97617cc066800272ef057d47ded7c
Apostolovicka <apostolovicka@gmail.com>
2016-07-17 18:57:53 +0200

    commit

---
+++ src/graphic/DDoSSimulation.java
@@ -0,0 +1,53 @@
+package graphic;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import processing.core.PApplet;
+
+
+public class DDoSSimulation {
+
+	private JFrame window;
+	private JPanel configurePanel, simulationPanel;
+	private ProcessingSimulation sim;
+	
+	public DDoSSimulation() {
+		makeWindow();
+		runSimulation();
+	}
+	
+	private void makeWindow() {
+		window = new JFrame("DDoS simulation");
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		window.setLayout(new BorderLayout());
+		
+		generateTabs();
+		
+		window.setVisible(true);
+		window.setResizable(false);
+	}
+	
+	public void runSimulation() {
+		String[] pArgs = {"ProcessingSimulation "};
+	    sim = new ProcessingSimulation();
+	    PApplet.runSketch(pArgs, sim);
+	}
+	
+	private void generateTabs() {
+		JTabbedPane tabs = new JTabbedPane();
+		simulationPanel = new JPanel();
+		configurePanel = new JPanel();
+		tabs.addTab("Simulation", simulationPanel);
+		tabs.addTab("Configure", configurePanel);
+		window.add(tabs);
+	}
+	
+	public static void main(String[] args) {
+		DDoSSimulation mainThread = new DDoSSimulation();
+	}
+
+}
